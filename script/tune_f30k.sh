4e9d8fca37841762e52a9ba9be002362e4e5037d
idejie <i@idejie.com>
2020-10-23 17:05:13 +0800

    Update tune_f30k.sh

--- script/tune_f30k.sh
+++ script/tune_f30k.sh
@@ -1,9 +1,9 @@
 data_name=f30k_precomp
 Batch_size=128
-DATA_PATH=../../../data
+DATA_PATH=../data
 MODEL_DIR=checkpoint/f30k_scan/
 
-GPU_ID="1,2"
+GPU_ID="0"
 iteration_step=1
 lambda_softmax=9
 model_mode=image_IMRAM #full_IMRAM, text_IMRAM, image_IMRAM
@@ -15,7 +15,7 @@ model_name=${logger_name}
 
 echo ${logger_name}
 
-python train_gpus.py --gpuid ${GPU_ID} --batch_size ${Batch_size} --data_path ${DATA_PATH} --data_name ${data_name} --vocab_path vocab --logger_name ${logger_name} --model_name ${model_name} --max_violation --bi_gru --agg_func=Mean --lambda_softmax=${lambda_softmax} --model_mode ${model_mode} --iteration_step ${iteration_step} --no_IMRAM_norm
+python train_gpus.py --gpuid ${GPU_ID} --batch_size ${Batch_size} --data_path ${DATA_PATH} --data_name ${data_name} --vocab_path ../vocab --logger_name ${logger_name} --model_name ${model_name} --max_violation --bi_gru --agg_func=Mean --lambda_softmax=${lambda_softmax} --model_mode ${model_mode} --iteration_step ${iteration_step} --no_IMRAM_norm
 
 echo "---------------evaluation--------------"
 MODEL_PATH=${model_name}/model_best.pth.tar
